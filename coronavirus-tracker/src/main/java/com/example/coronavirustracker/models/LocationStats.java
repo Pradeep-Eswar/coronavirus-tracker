2425f1f39cdc98a4bfe13907d06cba8378aac5a1
pradeep.eswaran <35631568+Pradeep-Eswar@users.noreply.github.com>
2020-03-27 21:06:39 +0100

    Initial Commit

---
+++ coronavirus-tracker/src/main/java/com/example/coronavirustracker/models/LocationStats.java
@@ -0,0 +1,46 @@
+package com.example.coronavirustracker.models;
+
+public class LocationStats {
+
+
+    private String state;
+    private String Country;
+    private int latestTotalCases;
+    private int diffFromPrevDay;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
+    }
+
+
+
+
+}
